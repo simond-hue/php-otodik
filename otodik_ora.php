3624e0e68a9e5fafe681546199497b3da3537ef8
simond-hue <achipowa205@gmail.com>
2019-10-04 07:41:33 +0200

    elso

---
+++ otodik_ora.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<html>
+<head>
+	<style type="text/css">
+		*{
+			background-color:#222222;
+			color:#FFFFFF;
+		}
+	</style>
+	<title>Ötödik óra</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+</body>
+</html>
